ed975be06516a4756788046e129a88bf67244932
Jundi <indana6969@gmail.com>
2021-12-27 14:33:25 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const openBtn = document.getElementById('open-btn')
+const closeBtn = document.getElementById('close-btn')
+const navbar = document.getElementById('navbar')
+
+openBtn.addEventListener('click', () => {
+    navbar.style.backgroundColor = '#fff'
+    navbar.classList.add('show')
+})
+
+closeBtn.addEventListener('click', () => {
+    navbar.classList.remove('show')
+    navbar.style.backgroundColor = 'transparent'
+})
+
+window.onscroll = function() {
+    if (window.scrollY > 0) {
+        navbar.style.backgroundColor = '#fff'
+    } else {
+        navbar.style.backgroundColor = 'transparent'
+    }
+}
